37917b993920c3f9b7a8f76c8f9fba41b0c8233a
wilbaez <101609902+wilbaez@users.noreply.github.com>
2022-03-14 22:35:07 -0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -21,6 +21,7 @@ CREATE TABLE employees (
   first_name      VARCHAR(32) NOT NULL,
   last_name       VARCHAR(32) NOT NULL,
   position        VARCHAR(32) NOT NULL,
+  lvl             VARCHAR(16) NOT NULL,
   PRIMARY KEY     ( employee_id )
 );
 
